b5e70aba6f91c7cc8f2ab0937c4d14d5b09cf21a
williamrackley <willrackley65@gmail.com>
2019-02-19 11:43:24 -0500

    started styling for modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,6 +4,8 @@ var friends = [
     {
         "name":"Ahmed",
         "photo":"http://www.free-largeimages.com/wp-content/uploads/2014/12/Happy_person-9.jpg",
+        "age": 22,
+        "location": "Long Island, NY",
         "scores":[
             5,
             1,
@@ -20,6 +22,8 @@ var friends = [
     {
         "name":"Sam",
         "photo":"http://natashatracy.com/blog/wp-content/uploads/2012/11/MP90044342111.jpg",
+        "age": 27,
+        "location": "Buffalo, NY",
         "scores":[
             4,
             1,
