3df49cbb9184af610e01bd04cda1d1a370647ce0
williamrackley <willrackley65@gmail.com>
2019-02-20 10:30:08 -0500

    correctly connected routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,21 +7,7 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-    //route to obtain css file 
-    app.get("/style.css", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/style.css"));
-    });
-
-    //route to obtain image file 
-    app.get("/friends.jpg", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/friends.jpg"));
-    });
-
-    //home page route
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-
+    
     //catch all route to home page
     app.get('*',function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 var express = require("express");
-var path = require("path");
+//var path = require("path");
 var app = express();
-
-require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
-require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static('./app/public'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//static directory
+app.use(express.static('./app/public'));
 
+//Routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("Friends App listening on PORT " + PORT);
 });
 
 
