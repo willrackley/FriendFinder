eea1bdff8b6a4f7d8b72f6dbab19fd190d0842e5
williamrackley <willrackley65@gmail.com>
2019-02-20 20:49:24 -0500

    added styling to home and survey pages

--- app/data/friends.js
+++ app/data/friends.js
@@ -36,6 +36,42 @@ var friends = [
             1,
             1
         ]
+    },
+    {
+        "name":"Corey",
+        "photo":"https://files.insidify.com/uploads/discovery/happy-black-man.png",
+        "age": 21,
+        "location": "Atlanta, Ga",
+        "scores":[
+            1,
+            2,
+            4,
+            3,
+            5,
+            1,
+            2,
+            4,
+            1,
+            1
+        ]
+    },
+    {
+        "name":"Briana",
+        "photo":"https://media1.popsugar-assets.com/files/thumbor/n8QtCsGJvt2JlVjqAV2EGnHmdIo/fit-in/550x550/filters:format_auto-!!-:strip_icc-!!-/2017/11/01/802/n/1922441/17bad3c559fa0f5e2f9c49.57527930_edit_img_image_44206058_1509556623/i/What-Makes-Person-Happy.jpg",
+        "age": 25,
+        "location": "Hampton, Va",
+        "scores":[
+            5,
+            3,
+            2,
+            1,
+            5,
+            1,
+            2,
+            4,
+            3,
+            1
+        ]
     }
 ];
     
