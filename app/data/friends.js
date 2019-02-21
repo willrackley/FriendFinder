20ed20cfd9b48964ecfc685a0d67e0e935f51897
williamrackley <willrackley65@gmail.com>
2019-02-21 10:08:40 -0500

    added entries to api List

--- app/data/friends.js
+++ app/data/friends.js
@@ -41,7 +41,7 @@ var friends = [
         "name":"Corey",
         "photo":"https://files.insidify.com/uploads/discovery/happy-black-man.png",
         "age": 21,
-        "location": "Atlanta, Ga",
+        "location": "Atlanta, GA",
         "scores":[
             1,
             2,
@@ -59,7 +59,7 @@ var friends = [
         "name":"Briana",
         "photo":"https://media1.popsugar-assets.com/files/thumbor/n8QtCsGJvt2JlVjqAV2EGnHmdIo/fit-in/550x550/filters:format_auto-!!-:strip_icc-!!-/2017/11/01/802/n/1922441/17bad3c559fa0f5e2f9c49.57527930_edit_img_image_44206058_1509556623/i/What-Makes-Person-Happy.jpg",
         "age": 25,
-        "location": "Hampton, Va",
+        "location": "Hampton, VA",
         "scores":[
             5,
             3,
@@ -71,6 +71,114 @@ var friends = [
             4,
             3,
             1
+        ]
+    },
+    {
+        "name":"Mike",
+        "photo":"https://www.1dental.com/blog/wp-content/uploads/2016/03/man.jpg",
+        "age": 24,
+        "location": "Los Angeles, CA",
+        "scores":[
+            2,
+            3,
+            5,
+            3,
+            1,
+            2,
+            2,
+            4,
+            5,
+            1
+        ]
+    },
+    {
+        "name":"Marie",
+        "photo":"http://www.thedentalboutique.ae/wp-content/uploads/2015/07/maria-anderson.jpg",
+        "age": 27,
+        "location": "Austin, TX",
+        "scores":[
+            4,
+            4,
+            2,
+            2,
+            1,
+            3,
+            3,
+            5,
+            1,
+            5
+        ]
+    },
+    {
+        "name":"Thomas",
+        "photo":"https://media.istockphoto.com/photos/portrait-of-smiling-young-man-outdoors-picture-id478022628?k=6&m=478022628&s=612x612&w=0&h=BVgsN1HIYlkfbq1v2kfwcxKPW5rx9GPU3AY25CZD2SI=",
+        "age": 24,
+        "location": "Jacksonville, FL",
+        "scores":[
+            1,
+            4,
+            5,
+            5,
+            5,
+            2,
+            1,
+            2,
+            4,
+            1
+        ]
+    },
+    {
+        "name":"Tonya",
+        "photo":"https://caitlinpascoe.files.wordpress.com/2011/05/african-american-woman.jpg",
+        "age": 29,
+        "location": "Columbia, SC",
+        "scores":[
+            1,
+            2,
+            2,
+            3,
+            3,
+            5,
+            5,
+            2,
+            1,
+            1
+        ]
+    },
+    {
+        "name":"Chris",
+        "photo":"https://il4.picdn.net/shutterstock/videos/1583458/thumb/3.jpg",
+        "age": 22,
+        "location": "Phoenix, AZ",
+        "scores":[
+            3,
+            2,
+            3,
+            4,
+            1,
+            5,
+            2,
+            2,
+            4,
+            1
+        ]
+    },
+    {
+        "name":"Katy",
+        "photo":"https://c.pxhere.com/images/49/ce/1aa023be2db2dd135006212751eb-1419067.jpg!d",
+        "age": 25,
+        "location": "Canton, OH",
+        "scores":[
+            1,
+            5,
+            3,
+            5,
+            2,
+            1,
+            2,
+            3,
+            4,
+            2
         ]
     }
 ];
