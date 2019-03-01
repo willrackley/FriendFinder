578e6e29ead81aace484f809ab84830e94a08fc5
williamrackley <willrackley65@gmail.com>
2019-03-01 12:21:08 -0500

    fixed backend code

--- app/public/js/logic.js
+++ app/public/js/logic.js
@@ -5,65 +5,21 @@ $("#form").submit(function(event){
     event.preventDefault(); 
     
     var userSurveyArray = [];
-    var userComparisonArray = [];
-    var allFriendScores = [];
-    var dataCtr = 0;
-    var matchIndex = 0;
-
-    //once we have all differences from the user and a person from the friends list we then get the the total difference
-    function totalDiff(array) {
-        
-        var add = (a, b) =>
-            a + b
-            var totalDiff = array.reduce(add);
-
-            allFriendScores.push(totalDiff);
-            console.log("all "+ allFriendScores);
-            console.log(Math.min(...allFriendScores));
-            var lowestDiff = Math.min(...allFriendScores);
-            matchIndex = allFriendScores.indexOf(lowestDiff)
-    }
-
-    //this function is used to display the users friend match in the modal
-    function matchDisplay(data, index){
-        $("#modalName").text(data[index].name + ", " + data[index].age);
-        $("#modalLocation").text("" + data[index].location);
-        $("#modalImg").attr("src", data[index].photo);
-        $('.modal').modal('show');
-    }
-
-    //this function uses a git request to get info from the friends api list
-    function getFriendScores(){
-        $.get("/api/friends", function(data) {
-        
-        console.log(data);
-       
-            if (data) {
-                if(dataCtr < data.length){
-                    userComparisonArray = [];
-                    for(var i=0; i < userSurveyArray.length; i++){
-                        answerDiff = Math.abs(userSurveyArray[i] - data[dataCtr].scores[i]);
-                        userComparisonArray.push(answerDiff);
-                    }
-                    console.log("uCA " + userComparisonArray);
-                    totalDiff(userComparisonArray);
-                    dataCtr++;
-                    matchDisplay(data, matchIndex);
-                    getFriendScores();
-                    
-                    } 
-                }
-        });
-    }
 
     //use a for loop to place the user's survey answers in an array
     for(var i=1; i < 11; i++){
         var question = "#question" + i + " :selected";
         var userChoice = $(question).text().split(" (");
-        userSurveyArray.push(userChoice[0])
+        userSurveyArray.push(userChoice[0]) 
     }
-    console.log(userSurveyArray);
-    getFriendScores();
+
+    var user = {
+        "name": $('#nameInput').val().trim(),
+        "photo":$('#photoInput').val().trim(),
+        "age": $('#ageInput').val().trim(),
+        "location": $('#locationInput').val(),
+        "scores":userSurveyArray
+    };
 
     //clearing selected form values
     for(var i=0; i < 10; i++){
@@ -74,5 +30,18 @@ $("#form").submit(function(event){
     $('#photoInput').val('');
     $('#locationInput').val('');
     $('#ageInput').val('');
+
+    
+
+    $.post("/api/friends", user, function(data) {
+        console.log("post fired")
+        $('#showResult').append(data);
+        $('.modal').modal('show');
+    });
+
+    $("#showResult").on("hidden.bs.modal", function () {
+        window.location.href = "/";
+    });
+   
 });
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,11 +8,75 @@ module.exports = function(app){
     
     app.post("/api/friends", function(req, res) {
         
-        var newUserInfo = req.body;
-    
-        newUserInfo.routeName = newUserInfo.name.replace(/\s+/g, "").toLowerCase();
-      
-        console.log(newUserInfo);
-      });
+        var newUser = req.body;
+        var userComparisonArray = [];
+        var allFriendScores = [];
+        var dataCtr = 0;
+        var matchIndex = 0;
+        var matchIndexArray = [];
+        
+
+        //once we have all differences from the user and a person from the friends list we then get the the total difference
+        function totalDiff(array) {
+            var add = (a, b) =>
+                a + b
+                var totalDiff = array.reduce(add);
+
+            allFriendScores.push(totalDiff);
+            console.log("all "+ allFriendScores);
+            console.log(Math.min(...allFriendScores));
+            var lowestDiff = Math.min(...allFriendScores);
+            matchIndex = allFriendScores.indexOf(lowestDiff)
+            matchIndexArray.push(matchIndex);
+        }
+
+        //this function uses a git request to get info from the friends api list
+        function getFriendScores(){
+            if (friends !== []) {
+                if(dataCtr < friends.length){
+                    userComparisonArray = [];
+                    for(var i=0; i < newUser.scores.length; i++){
+                        var answerDiff = Math.abs(newUser.scores[i] - friends[dataCtr].scores[i]);
+                        userComparisonArray.push(answerDiff);
+                    }
+                    totalDiff(userComparisonArray);
+                    dataCtr++;
+                    getFriendScores();
+                }   
+            }         
+        }
+        
+        getFriendScores();
+        var finalIndex = matchIndexArray[matchIndexArray.length -1];
+        var matchName = friends[finalIndex].name;
+        var matchAge = friends[finalIndex].age;
+        var matchLocation = friends[finalIndex].location;
+        var matchImg = friends[finalIndex].photo;
+
+        var matchModal = 
+        `<div class="modal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class='modal-title w-100 text-center'>FRIEND <i class="fab fa-searchengin"></i> FINDER</h3>
+                </div>
+                <div class="modal-body">
+                    <h4 class='mb-4 text-center'>We've found a great friend for you!</h4>
+                    <h3 id="modalName">${matchName}, ${matchAge}</h3> 
+                    <p></p>
+                    <img id="modalImg" src="${matchImg}" class="img-fluid" alt="picture of new friend">
+                    <p></p>
+                    <h4 id="modalLocation">${matchLocation} </h4>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-dark" data-dismiss="modal">Close</button>
+                </div>
+                </div>
+            </div>
+        </div>`
+
+        friends.push(newUser);
+        return res.send(matchModal);
+    });
 };
 
